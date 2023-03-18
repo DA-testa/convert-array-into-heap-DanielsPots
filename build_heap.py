87b09ffd783843947edabe52bf4f23306cb37403
DanielsPots <124889596+DanielsPots@users.noreply.github.com>
2023-03-18 12:24:33 +0000

    commit

--- build_heap.py
+++ build_heap.py
@@ -32,11 +32,20 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    input_type = input("Enter input type (I - from keyboard or F - from file): ").strip()
+    if input_type == "I":
+        # input from keyboard
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif input_type == "F":
+        test_number = input("Enter test number (e.g. 04): ")
+        test_path = f"/workspaces/convert-array-into-heap-DanielsPots/tests/{test_number}"
+        with open(test_path, "r") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    else:
+        print("Invalid input type")
+        return
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
